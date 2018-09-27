e55026979a6802f0dbb8328f9feb476a2da288cc
joemissamore <joemissamore@live.com>
2018-09-26 21:01:26 -0700

    errors are being reported, cleaned up code

---
+++ Project1.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include "SetLimits.h"
+#include "SyntacticalAnalyzer.h"
+
+int main (int argc, char * argv[])
+{
+	if (argc < 2)
+	{
+		printf ("format: proj1 <filename>\n");
+		exit (1);
+	}
+	SetLimits ();
+	// SyntacticalAnalyzer parser (argv[1]);
+	LexicalAnalyzer parser (argv[1]);
+
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# cs460P1
+
+The table is working pretty well its now reporting errors and the char causing the error.
+TODO: Write out err, rewrite conditional loop so syntactical analyzer has control over it.
